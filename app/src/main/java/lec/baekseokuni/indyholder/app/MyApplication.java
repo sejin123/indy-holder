a380a340159a7cb11b4f4a49ba85c38eab8d1db1
김소현 <sohyun3490@bdgen.co.kr>
2023-07-15 09:40:47 +0900

    style: class name 수정

--- app/src/main/java/lec/baekseokuni/indyholder/app/MyApplication.java
+++ app/src/main/java/lec/baekseokuni/indyholder/app/MyApplication.java
@@ -2,15 +2,12 @@ package lec.baekseokuni.indyholder.app;
 
 import android.app.Application;
 
-import org.hyperledger.indy.sdk.IndyException;
 import org.hyperledger.indy.sdk.pool.Pool;
-
-import java.util.concurrent.ExecutionException;
 
 import kr.co.bdgen.indywrapper.IndyWrapper;
 import kr.co.bdgen.indywrapper.config.PoolConfig;
 
-public class App extends Application {
+public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
