2b8dee346e00540d88f09c784a33bb5a84e008d4
김소현 <sohyun3490@bdgen.co.kr>
2023-07-17 15:28:01 +0900

    fix: 병합 충돌 최소화하기 위한 code line 제거

--- app/src/main/java/lec/baekseokuni/indyholder/MyApplication.java
+++ app/src/main/java/lec/baekseokuni/indyholder/MyApplication.java
@@ -2,22 +2,9 @@ package lec.baekseokuni.indyholder;
 
 import android.app.Application;
 
-import org.hyperledger.indy.sdk.pool.Pool;
-
-import kr.co.bdgen.indywrapper.IndyWrapper;
-import kr.co.bdgen.indywrapper.config.PoolConfig;
-
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        IndyWrapper.init(this);
-        String poolName = PoolConfig.getPoole(this);
-        try {
-            Pool.openPoolLedger(poolName, "{}").get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
