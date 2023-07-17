c212b8655d6c1fbacb25983c91ef838b863246b0
김소현 <sohyun3490@bdgen.co.kr>
2023-07-17 10:39:33 +0900

    feat: 각 기능 별로 activity 분리

---
+++ app/src/main/java/lec/baekseokuni/indyholder/issue/IssueActivity.java
@@ -0,0 +1,16 @@
+package lec.baekseokuni.indyholder.issue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import lec.baekseokuni.indyholder.R;
+
+public class IssueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_issue);
+    }
+}
---
+++ app/src/main/java/lec/baekseokuni/indyholder/verification/VerificationActivity.java
@@ -0,0 +1,16 @@
+package lec.baekseokuni.indyholder.verification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import lec.baekseokuni.indyholder.R;
+
+public class VerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
