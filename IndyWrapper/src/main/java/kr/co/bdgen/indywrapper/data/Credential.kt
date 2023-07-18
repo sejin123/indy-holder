0e216e5d50201f859d6649238d128b9929558fde
김소현 <sohyun3490@bdgen.co.kr>
2023-07-18 14:31:04 +0900

    fix: json 키 이름과 객체 명이 달라 설명 추가

--- IndyWrapper/src/main/java/kr/co/bdgen/indywrapper/data/Credential.kt
+++ IndyWrapper/src/main/java/kr/co/bdgen/indywrapper/data/Credential.kt
@@ -1,14 +1,19 @@
 package kr.co.bdgen.indywrapper.data
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
+import java.io.Serializable
 
+/**
+ * json 내 key: cred_info
+ */
 @Parcelize
 data class Credential(
     val referent: String,
-    val schema_id: String,
-    val cred_def_id: String,
-    val rev_reg_id: String? = null,
-    val cred_rev_id: String? = null,
+    @SerializedName("schema_id") val schemaId: String,
+    @SerializedName("cred_def_id") val credDefId: String,
+    @SerializedName("rev_reg_id") val revRegId: String? = null,
+    @SerializedName("cred_rev_id") val credRevId: String? = null,
     val attrs: MutableMap<String, String>? = null,
-) : Parcelable
+) : Parcelable, Serializable
--- IndyWrapper/src/main/java/kr/co/bdgen/indywrapper/data/argument/CredentialInfo.kt
+++ IndyWrapper/src/main/java/kr/co/bdgen/indywrapper/data/argument/CredentialInfo.kt
@@ -1,5 +1,8 @@
 package kr.co.bdgen.indywrapper.data.argument
 
+/**
+ * credentialRequest object for sdk
+ */
 data class CredentialInfo(
     val credDefJson: String,
     val credReqMetadataJson: String,
