939fafe79ddcf82f25806d734922ab1748435b03
jhrac <joohyeong2001@gmail.com>
2023-07-22 11:37:31 +0900

    activity_main 메인 화면 디자인 추가 수정

--- app/src/main/java/lec/baekseokuni/indyholder/MainActivity.java
+++ app/src/main/java/lec/baekseokuni/indyholder/MainActivity.java
@@ -14,7 +14,7 @@ import lec.baekseokuni.indyholder.credential.CredentialListActivity;
 
 public class MainActivity extends AppCompatActivity {
     View.OnClickListener onStartDeeplink = v -> {
-        String testDeeplink = "indy://holder?secret=test1";
+        String testDeeplink = "indy://holder?secret=4cvmHr8gZI";
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(testDeeplink));
         startActivity(intent);
         finish();
