cee7081d4a2aadb14d2bf6ae9ee5ec3b296e7c5a
sejin123 <ssjsam33@gmail.com>
2023-07-22 16:17:46 +0900

    메인 화면에 gif이미지 추가

--- app/src/main/java/lec/baekseokuni/indyholder/MainActivity.java
+++ app/src/main/java/lec/baekseokuni/indyholder/MainActivity.java
@@ -5,10 +5,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 import lec.baekseokuni.indyholder.credential.CredentialListActivity;
 
@@ -28,9 +31,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView background = findViewById(R.id.main_background);
         Button btnIssueTest = findViewById(R.id.btn_issue_test_cred);
         Button btnNavToCredList = findViewById(R.id.btn_nav_to_cred_list);
         btnIssueTest.setOnClickListener(onStartDeeplink);
         btnNavToCredList.setOnClickListener(onNavToCredList);
+
+        Glide.with(this).load(R.drawable.finger).into(background);
     }
 }
